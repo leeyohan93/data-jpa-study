de53016cf998c421032bd23500d51795fc5f5106
yohan <good_1411@naver.com>
2020-03-01 23:32:44 +0900

    memberRepository 기본 테스트

---
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package study.datajpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.datajpa.entity.Member;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByUsername(String username);
+}
